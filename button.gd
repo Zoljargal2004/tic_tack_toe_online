59b0ee745dadda1e6f176367caacdfde7ac00885
MrPresent7 <Erkhembileg1230@gmail.com>
2025-06-04 15:03:08 +0100

    SDx
    Sec

--- button.gd
+++ button.gd
@@ -2,6 +2,8 @@ extends Button
 
 @onready var sex: Sprite2D = $"../Icon"
 
-
-func _on_pressed() -> void:
+@rpc("any_peer")
+func Sex_time():
 	sex.transform *= 2
+func _on_pressed() -> void:
+	Sex_time.rpc()
