12db942f5999f1f415f67aa0646aa05017f6b90a
MrPresent7 <Erkhembileg1230@gmail.com>
2025-06-04 15:16:55 +0100

    Ha
    Ho

--- button.gd
+++ button.gd
@@ -1,9 +1,11 @@
 extends Button
 
 @onready var sex: Sprite2D = $"../Icon"
+@onready var Sexist: VideoStreamPlayer = $"../VideoStreamPlayer"
 
 @rpc("any_peer")
 func Sex_time():
 	sex.transform *= 2
+	Sexist.visible = not Sexist.visible
 func _on_pressed() -> void:
 	Sex_time.rpc()
